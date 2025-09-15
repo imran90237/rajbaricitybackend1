010c96331c898ff0e7fd7b399e2c052eea70be25
imran <imran90237@gmail.com>
2025-09-15 06:41:10 +0600

    jobtraining

--- src/main/kotlin/com/example/rajbaricity/repository/JobInfoRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rajbaricity.repository
-
-import com.example.rajbaricity.model.JobInfo
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface JobInfoRepository : JpaRepository<JobInfo, Long>
--- src/main/kotlin/com/example/rajbaricity/repository/TrainingInfoRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rajbaricity.repository
-
-import com.example.rajbaricity.model.TrainingInfo
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TrainingInfoRepository : JpaRepository<TrainingInfo, Long>
